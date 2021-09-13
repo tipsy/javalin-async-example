26377ca183b78888e21bb85d05ed1fa9f830a561
David <davidaase@hotmail.com>
2021-09-13 15:38:12 +0200

    bump

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,6 +1,7 @@
 package app;
 
 import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -15,18 +16,22 @@ public class Main {
 
         Javalin app = Javalin.create(config -> {
             config.server(() -> ServerUtil.createHttp2Server(new QueuedThreadPool(10, 2, 60_000)));
-            config.addStaticFiles("/public");
+            config.addStaticFiles("/public", Location.CLASSPATH);
         }).start();
 
         app.get("/async", ctx -> {
-            long taskTime = ctx.queryParam("task-time", Long.class).get();
-            ctx.result(getFuture(taskTime));
+            long taskTime = ctx.queryParamAsClass("task-time", Long.class).get();
+            ctx.future(getFuture(taskTime));
         });
 
         app.get("/blocking", ctx -> {
-            long taskTime = ctx.queryParam("task-time", Long.class).get();
+            long taskTime = ctx.queryParamAsClass("task-time", Long.class).get();
             Thread.sleep(taskTime);
             ctx.result("done");
+        });
+
+        app.exception(Exception.class, (exception, ctx) -> {
+            System.out.println(exception.getMessage());
         });
 
     }
